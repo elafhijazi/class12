e9f71a78039263d2947d64f69fdc18fc989df42e
elafhijazi <elafhigazi7@gmail.com>
2024-09-24 13:23:55 +0300

    stam project :)

--- app/src/main/java/com/example/stam/MainActivity.java
+++ app/src/main/java/com/example/stam/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.stam;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,17 +12,41 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    public Button btnStart, btnEnd;
+    public TextView txtView;
+    public Integer num = 0;
+    Thread counterThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnStart = findViewById(R.id.btnStart);
+        btnEnd = findViewById(R.id.btnEnd);
+        txtView = findViewById(R.id.textView);
 
+    }
+
+    public void startCount(View view) {
+        counterThread = new Thread(() -> {
+            try {
+                while (true) {
+                    num++;
+                    txtView.setText(num + "");
+                    Thread.sleep(1000);
+                }
+
+            }
+            catch (Exception e){}
         });
+        counterThread.start();
+    }
+    public void endCount(View view){
+        counterThread.interrupt();
+    }
+    public void setTxtView(View view){
+        txtView.setText("0");
+        counterThread.interrupt();
+        num=0;
     }
 }
